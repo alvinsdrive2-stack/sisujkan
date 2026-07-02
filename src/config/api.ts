65d9bc57a39ceb10c8d4cdaddfba584e95ebaaad
alvinsdrive2-stack <alvins.drive2@gmail.com>
2026-07-02 14:33:45 +0700

    test kan

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-const KAN_API_URL = import.meta.env.VITE_API_BASE_URL_KAN || 'https://kan-api.lspgatensi.id'
+const KAN_API_URL = import.meta.env.VITE_API_BASE_URL_KAN || 'https://kan-api.lspgatensi.id/api'
 const DEFAULT_API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api'
 
 export const API_BASE_URL = import.meta.env.VITE_SAAT_INI === 'KAN' ? KAN_API_URL : DEFAULT_API_URL
